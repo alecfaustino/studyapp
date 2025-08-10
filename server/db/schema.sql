2942382d57e67b0affe3054528c58ae79b20a5f2
Alec Faustino <alecfaustino@outlook.com>
2025-08-10 12:54:12 -0600

    use pgcrypto extension to generate UUID instead of serial key

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,10 +1,13 @@
+-- Enable pgcrypto for UUID generation
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 -- table: users
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
-  password_hash VARCHAR(255) NOT NULL
+  password_hash VARCHAR(255) NOT NULL,
   profile_image_url TEXT,
   city VARCHAR(50),
   education_level VARCHAR(50),
@@ -13,24 +16,24 @@ CREATE TABLE users (
 
 -- table: groups
 CREATE TABLE groups (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(50) NOT NULL,
   description TEXT,
-  created_by INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  created_by UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 -- table: review tags
 CREATE TABLE review_tags (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(50) NOT NULL UNIQUE
 );
 
 -- table: user_subjects
 CREATE TABLE user_subjects (
-  id SERIAL PRIMARY KEY, 
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  subject VARCHAR(150)
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  subject VARCHAR(150) NOT NULL, 
+  PRIMARY KEY (user_id, subject)
 );
 
 -- enum for the role in group_users
@@ -38,8 +41,8 @@ CREATE TYPE role_enum AS ENUM ('member', 'admin');
 
 -- table: group_users
 CREATE TABLE group_users (
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  group_id INT NOT NULL REFERENCES groups(id) ON DELETE CASCADE, 
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  group_id UUID NOT NULL REFERENCES groups(id) ON DELETE CASCADE, 
   joined_at TIMESTAMPTZ DEFAULT NOW(),
   role role_enum NOT NULL DEFAULT 'member',
   PRIMARY KEY (user_id, group_id)
@@ -47,9 +50,9 @@ CREATE TABLE group_users (
 
 -- table: user_ratings
 CREATE TABLE user_ratings (
-  id SERIAL PRIMARY KEY, 
-  rated_user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  tag_id INT NOT NULL REFERENCES review_tags(id) ON DELETE CASCADE,
-  rating_user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+  rated_user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  tag_id UUID NOT NULL REFERENCES review_tags(id) ON DELETE CASCADE,
+  rating_user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ DEFAULT NOW()
 );
