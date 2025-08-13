bc8bfc7c25d04f9b7484c3210ea8ee19be9e1a1f
Alec Faustino <alecfaustino@outlook.com>
2025-08-13 13:33:52 -0600

    add bottom nav styles

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,16 +1,21 @@
+import '../styles/BottomNav.css'
 const BottomNav = () => {
 
   // TODO change the li to icons and use Link react Routes
   return (
     <>
-      <ul>
-        <li>Chat</li>
-        <li>Browse Profiles</li>
-        <li>TBD</li>
-        {/* profile would be the logged in person's photo? */}
-        {/* initial working copy maybe just a stock profile button */}
-        <li>Profile</li>
-      </ul>
+      <div className='navbar'>
+
+        <ul className='navlist'>
+          <li>Chat</li>
+          <li>Browse Profiles</li>
+          <li>TBD</li>
+          {/* profile would be the logged in person's photo? */}
+          {/* initial working copy maybe just a stock profile button */}
+          <li>Profile</li>
+        </ul>
+
+      </div>
     
     </>
   )
