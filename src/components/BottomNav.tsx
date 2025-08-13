81b191db4b19a111d9d16f45ab70f54a1cd3407f
Alec Faustino <alecfaustino@outlook.com>
2025-08-13 14:32:59 -0600

    add two buttons to the bottom nav and outline what they should do

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -9,7 +9,12 @@ const BottomNav = () => {
         <ul className='navlist'>
           <li>Chat</li>
           <li>Browse Profiles</li>
-          <li>TBD</li>
+          {/* Start Study Session should be a new flow into a page where you can initiate a study session with someone or a solo study session */}
+          {/* A solo study Session initiates a pomodoro timer type thing */}
+          {/* Either a group or a solo study session should be counting towards their streak */}
+          {/* A streak (like github commit map) should be on their profile */}
+          <li>Start Study Session</li>
+          <li>Notifications</li>
           {/* profile would be the logged in person's photo? */}
           {/* initial working copy maybe just a stock profile button */}
           <li>Profile</li>
