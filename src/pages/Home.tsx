c6fbef5a4ab595a1ef1435701a98555ca81f0e40
Alec Faustino <alecfaustino@outlook.com>
2025-08-13 14:05:00 -0600

    add css reset and some main page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import BottomNav from "../components/BottomNav"
+import '../styles/Home.css'
+import templogo from '../assets/templogo.png'
 
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <header className="home-header">
+        <img alt="temporary logo" src={templogo} className="home-logo" />
+        <h2>StudyAppNameTBD</h2>
+      </header>
+
+      <main>
+        <h3>Recent Study Events From Your Network</h3>
+        {/* TO DO Make the article a component */}
+        <article className="home-article">
+          <p>Alec Faustino</p>
+          {/* This means, we need a user-event table maybe? */}
+          <p>Alec Faustino had a study event with John Doe</p>
+        </article>
+      </main>
       <BottomNav />
     </>
   )
